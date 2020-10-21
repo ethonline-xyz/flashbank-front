6e02c3b9ffbc6daa4b8e9421853d03b18173b2aa
Thrilok Kumar <thrilok2000@gmail.com>
2020-10-22 05:10:05 +0530

    Done with deposit underlying function

--- src/components/InputFields/AddUnderlyingTokens.tsx
+++ src/components/InputFields/AddUnderlyingTokens.tsx
@@ -1,35 +1,61 @@
 import React, { useState } from "react";
+import cERC20PoolABI from "../cERC20Pool.json";
 // import cERC20PoolABI from "../cERC20Pool.json";
 // import flashLoadModuleABI from "../flashLoadModule.json";
 
 // hooks and services
 import { useStoreState } from "../../store/globalStore";
+import { AddressOfContract } from "../addresses";
 
 // components, styles and UI
 import { Button, Input } from "semantic-ui-react";
 import Swal from "sweetalert2";
+import { toast } from "react-toastify";
 
 // interfaces
 export interface AddUnderlyingTokensProps {}
 
 const AddUnderlyingTokens: React.FunctionComponent<AddUnderlyingTokensProps> = () => {
-  const { web3 } = useStoreState((state) => state);
+  const { web3, account, selectedToken } = useStoreState((state) => state);
+
 
   const [value, setValue] = useState<string>("");
 
   const handleSubmit = () => {
-    if (value) {
-      // make changes here for deposit
-      console.log(value);
-      setValue("");
-    } else {
-      Swal.fire("err...", "Please enter a value", "warning");
-    }
+    let token = selectedToken;
+    if (!token) return Swal.fire("err...", "Please select token", "warning");
+    if (!value) return Swal.fire("err...", "Please enter a value", "warning");
+    var contractInstance = new web3.eth.Contract(
+      cERC20PoolABI,
+      AddressOfContract.ctokenPools[token.toLowerCase()]
+    );
+    let valueInWei = String((Number(value) * 10 ** 8).toFixed(0));
+    contractInstance.methods
+      .depositUnderlying(valueInWei)
+      .send({
+        from: account,
+      })
+      .on("transactionHash", function (hash) {
+        Swal.fire(
+          "Tx pending",
+          `view on <a target="_blank" rel = "noopener noreferrer" href='https://kovan.etherscan.io/tx/${hash}'>etherscan</a>`,
+          "success"
+        );
+        setValue("");
+      })
+      .on("receipt", function (receipt) {
+        toast(`Transaction Confirmed (view)`, {
+          onClick: () =>
+            window.open(
+              `https://kovan.etherscan.io/tx/${receipt.transactionHash}`
+            ),
+        });
+      });
   };
 
   return (
     <div className="entry-form">
-      <h4>Add Underlying Tokens</h4>
+      <h4>Deposit Underlying cToken</h4>
       <Input
         fluid
         placeholder="Amount"
